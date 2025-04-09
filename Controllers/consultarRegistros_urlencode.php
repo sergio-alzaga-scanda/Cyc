fddb427eb06e8c056299fda5ab08f83af9c91a7f
Sergio Alzaga <sergio.alzaga@scanda.com.mx>
2025-04-09 13:29:42 -0600

    mOD

--- Controllers/consultarRegistros_urlencode.php
+++ Controllers/consultarRegistros_urlencode.php
@@ -57,6 +57,7 @@ SELECT
 FROM [contingencias].[dbo].[cyc] AS cyc
 INNER JOIN [usuarios] AS u ON cyc.id_usuario = u.idUsuarios
 WHERE cyc.proyecto = ? AND cyc.ubicacion_cyc = ?
+AND cyc.status_cyc >= 0;
 ";
 
 try {
