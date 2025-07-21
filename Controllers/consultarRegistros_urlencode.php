05ea8b2237f37e0c1b87e0c09661d551c4f4f5f4
Sergio Alzaga <sergio.alzaga@scanda.com.mx>
2025-07-21 12:40:17 -0600

    Mod

--- Controllers/consultarRegistros_urlencode.php
+++ Controllers/consultarRegistros_urlencode.php
@@ -1,21 +1,28 @@
 <?php
 header('Content-Type: application/json');
 
-// Confirmar método GET
+// Log para saber qué método se está usando
+error_log('Método usado: ' . $_SERVER['REQUEST_METHOD']);
+
+// Mostrar variables GET recibidas (para depuración)
+error_log('$_GET completo: ' . print_r($_GET, true));
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Recuperar los parámetros
+    // Usamos el operador null coalescing
     $proyecto = $_GET['proyecto'] ?? null;
     $ubicacion = $_GET['ubicacion'] ?? null;
 
-    // Retornar JSON con los datos
+    // Log de valores individuales
+    error_log("proyecto: [$proyecto], ubicacion: [$ubicacion]");
+
     echo json_encode([
         'proyecto' => $proyecto,
         'ubicacion' => $ubicacion
     ]);
     exit;
 } else {
-    // Método no permitido
     http_response_code(405);
     echo json_encode(['error' => 'Método no permitido. Usa GET.']);
     exit;
 }
+?>
